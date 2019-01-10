a6ceafb655384a69ec6075512d64f03c99c5afe2
JerakRus <jerakrus@gmail.com>
2019-01-11 01:37:31 +0300

    store Refit Store

--- src/actions/asyncActions.js
+++ src/actions/asyncActions.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { createAction } from 'redux-actions';
 import axios from 'axios';
 
-//I get a list of parts from the server, I add to the state and create a list of models parts
+//I get a list of parts from the server, I add to the state and create a list of models parts.
+//It is used to display the filter for all available models.
 export const setPartsRequest = createAction('SET_PARTS_REQUEST');
 export const setPartsSuccess = createAction('SET_PARTS_SUCCESS');
 export const setPartsFailure = createAction('SET_PARTS_FAILURE');
@@ -25,7 +26,8 @@ export const setParts = () => async (dispatch) => {
     };
 };
 
-//I get a list of smartphones from the server, I add to the state and create a list of models smartphones
+//I get a list of smartphones from the server, I add to the state and create a list of models smartphones.
+//It is used to display the filter for all available models.
 export const setSmartphonesRequest = createAction('SET_SMARTPHONES_REQUEST');
 export const setSmartphonesSuccess = createAction('SET_SMARTPHONES_SUCCESS');
 export const setSmartphonesFailure = createAction('SET_SMARTPHONES_FAILURE');
