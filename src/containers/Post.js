59b92f775f265d6e721cf706a24c4362d316c55c
JerakRus <jerakrus@gmail.com>
2019-02-07 16:24:44 +0300

    v 1.1  добавлены посты на главную страницу c датой

--- src/containers/Post.js
+++ src/containers/Post.js
@@ -1,23 +1,10 @@
 import { connect } from 'react-redux';
 import Post from '../components/body/Post';
 
-const formatDate = date => {
-    let dd = date.getDate();
-    if (dd < 10) dd = '0' + dd;
-
-    let mm = date.getMonth() + 1;
-    if (mm < 10) mm = '0' + mm;
-
-    let yy = date.getFullYear() % 100;
-    if (yy < 10) yy = '0' + yy;
-
-    return dd + '.' + mm + '.' + yy;
-};
-const date = new Date();
 
 const mapStateToProps = (state, {post}) => {
     const props = {
-        date: formatDate(post.date || date),
+        date: post.date || '01.05.18' ,
         siteColor: state.siteColor,
     };
     return props;
